f43efc421f3b06130ee665cb00c95392c73949f5
Evandro Costa <evandro.souzac@gmail.com>
2024-03-12 21:49:01 -0300

    Create custom colors and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,10 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      green: "#4EE1A0",
+      gray: "#D9D9D9",
+      white: "#FFFFFF",
     },
   },
   plugins: [],
