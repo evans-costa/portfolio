7f5292c91e764f208d34bcb18720d43722c8e806
Evandro Costa <evandro.souzac@gmail.com>
2024-03-14 09:30:54 -0300

    Change language on html tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="pt-BR">
       <body className={spaceGrotesk.className}>{children}</body>
     </html>
   );
