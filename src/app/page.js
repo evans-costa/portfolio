286ab89d0e205f3a1536c2af43933b8dd619fdad
Evandro Costa <evandro.souzac@gmail.com>
2024-03-14 22:40:36 -0300

    Add Experience section on home

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,12 @@ import GitHubIcon from "../app/ui/github-icon";
 import LinkedInIcon from "../app/ui/linkedin-icon";
 import PatternRings from "../app/ui/pattern-rings";
 import ExperienceList from "../app/components/experienceList";
+import Link from "next/link";
+import { gitHubData } from "./lib/github-data";
 
-export default function Home() {
+export default async function Home() {
+  const repositoriesData = await gitHubData();
+
   return (
     <main className="mx-auto max-w-screen-xl min-h-screen flex-col items-center justify-between px-12 py-10">
       <header className="w-auto flex flex-col justify-between">
@@ -32,16 +36,27 @@ export default function Home() {
           </p>
         </div>
 
-        <span className="pb-3 w-fit text-white font-bold tracking-[0.15em] link link-underline text-base">
+        <Link
+          href="#projects"
+          className="cursor-pointer pb-3 w-fit text-white font-bold tracking-[2.3px] link link-underline text-base">
           CONTATO
-        </span>
+        </Link>
       </header>
 
-      <section className="relative mt-24 mb-36 pt-20 border-t border-white grid grid-cols-3 gap-y-14">
+      <section className="relative h-fit mt-24 mb-36 pt-20 border-t border-white grid grid-cols-3 content-start gap-y-14">
         <ExperienceList />
         <div className="absolute top-64 -right-[380px]">
           <PatternRings />
         </div>
+      </section>
+
+      <section className="flex justify-between items-center">
+        <h1 className="text-white">Projects</h1>
+        <Link
+          href="#projects"
+          className="cursor-pointer pb-3 w-fit text-white font-bold tracking-[0.15em] link link-underline text-base">
+          CONTATO
+        </Link>
       </section>
     </main>
   );
