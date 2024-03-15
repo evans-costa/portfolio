4447121c50de3c398f3eeadd2d93b6ac460a35ba
Evandro Costa <evandro.souzac@gmail.com>
2024-03-14 22:41:20 -0300

    Fix `fillRule` property on svg files

--- src/app/ui/pattern-rings.jsx
+++ src/app/ui/pattern-rings.jsx
@@ -1,7 +1,7 @@
 export default function PatternRings() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="530" height="129">
-      <g fill="none" fill-rule="evenodd" stroke="#FFF" opacity=".25">
+      <g fill="none" fillRule="evenodd" stroke="#FFF" opacity=".25">
         <ellipse cx="265" cy="40" rx="264.5" ry="39.5" />
         <ellipse cx="265" cy="52" rx="264.5" ry="39.5" />
         <ellipse cx="265" cy="65" rx="264.5" ry="39.5" />
