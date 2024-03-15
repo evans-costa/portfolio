3e081ae26d9d433ce688466022ba9634ae63ea9b
Evandro Costa <evandro.souzac@gmail.com>
2024-03-14 22:38:35 -0300

    Refine underline animation on name

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    extend: {
+      keyframes: {
+        underline: {
+          "0%": { "background-size": "0 6px " },
+          "100%": { "background-size": "100% 6px" },
+        },
+      },
+      animation: {
+        underline: "underline 1s ease-in-out .2s forwards",
+      },
+    },
     colors: {
       green: "#4EE1A0",
       gray: "#D9D9D9",
