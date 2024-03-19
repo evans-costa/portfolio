3d51ed266fc7a66df7e77d07a5829299ddb278cc
Evandro Costa <evandro.souzac@gmail.com>
2024-03-18 22:44:06 -0300

    Separate header section and hero section

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import ExperienceList from "@/components/Experience/experienceList";
 import ProjectList from "@/components/Project/projectList";
 import Contact from "@/components/Contact";
 
+
 export default async function Home() {
   return (
     <main className="mx-auto overflow-x-hidden max-w-screen-xl min-h-screen flex-col items-center justify-between px-12 py-10 text-gray">
@@ -15,11 +16,17 @@ export default async function Home() {
             evandrocosta.dev
           </span>
           <span className="flex gap-8">
-            <GitHubIcon />
-            <LinkedInIcon />
+            <a className="text-white hover:text-green" href="https://github.com/evans-costa" target="_blank" rel="noreferrer">
+              <GitHubIcon />
+            </a>
+            <a className="text-white hover:text-green" href="https://www.linkedin.com/in/evandro-souzac/" target="_blank" rel="noreferrer">
+              <LinkedInIcon />
+            </a>
           </span>
         </div>
+      </header>
 
+      <section id="hero">
         <div className="w-[710px] relative flex flex-col items-start my-32">
           <div className="absolute -top-16 -left-[300px]">
             <PatternRings />
@@ -34,26 +41,32 @@ export default async function Home() {
             eficientes para desafios complexos.
           </p>
         </div>
-        <Contact name={"CONTATO"} section={"#projects"} />
-      </header>
+        <Contact name={"CONTATO"} section={"#contact"} />
+      </section>
 
-      <section className="relative h-fit mt-24 mb-36 pt-20 border-t border-white grid grid-cols-3 content-start gap-y-14">
+      <section
+        id="experiences"
+        className="relative h-fit mt-24 mb-36 pt-20 border-t border-white grid grid-cols-3 content-start gap-y-14">
         <ExperienceList />
         <div className="absolute top-64 -right-[400px]">
           <PatternRings />
         </div>
       </section>
 
-      <section className="flex flex-col justify-center my-36">
+      <section id="projects" className="flex flex-col justify-center my-36">
         <div className="flex justify-between items-center mb-20">
           <h1 className="text-white">Projects</h1>
-          <Contact name={"CONTATO"} section={"#projects"} />
+          <Contact name={"CONTATO"} section={"#contact"} />
         </div>
-
         <div className="grid grid-cols-2 gap-x-7 gap-y-16">
           <ProjectList />
         </div>
       </section>
+
+      <footer>
+
+      </footer>
+
     </main>
   );
 }
