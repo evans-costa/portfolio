ccda2197f0a9433edc525d8ac8995b8b63311a75
Evandro Costa <evandro.souzac@gmail.com>
2024-03-19 23:55:31 -0300

    Use Social component on main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,15 @@
-import GitHubIcon from "@/components/Icons/github-icon";
-import LinkedInIcon from "@/components/Icons/linkedin-icon";
 import PatternRings from "@/components/Icons/pattern-rings";
 
 import ExperienceList from "@/components/Experience/experienceList";
 import ProjectList from "@/components/Project/projectList";
 import Contact from "@/components/Contact";
-
+import Social from "@/components/Social/";
 
 export default async function Home() {
   return (
-    <main className="mx-auto overflow-x-hidden max-w-screen-xl min-h-screen flex-col items-center justify-between px-12 py-10 text-gray">
+    <main className="mx-auto overflow-x-hidden max-w-screen-xl min-h-screen flex-col items-center justify-between px-12 pt-10 text-gray">
       <header className="w-auto flex flex-col justify-between">
-        <div className="flex flex-row justify-between w-full">
-          <span className="text-white text-3xl tracking-[0.5px] font-bold">
-            evandrocosta.dev
-          </span>
-          <span className="flex gap-8">
-            <a className="text-white hover:text-green" href="https://github.com/evans-costa" target="_blank" rel="noreferrer">
-              <GitHubIcon />
-            </a>
-            <a className="text-white hover:text-green" href="https://www.linkedin.com/in/evandro-souzac/" target="_blank" rel="noreferrer">
-              <LinkedInIcon />
-            </a>
-          </span>
-        </div>
+        <Social />
       </header>
 
       <section id="hero">
@@ -62,11 +48,6 @@ export default async function Home() {
           <ProjectList />
         </div>
       </section>
-
-      <footer>
-
-      </footer>
-
     </main>
   );
 }
