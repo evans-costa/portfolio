cdfa9435be1613a787e3129440de9cf7afc47778
Evandro Costa <evandro.souzac@gmail.com>
2024-03-21 23:17:24 -0300

    Submit form logic and toast message

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -4,6 +4,9 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
+import toast, { Toaster } from "react-hot-toast";
+import Spin from "@/components/Icons/spin-icon";
+
 const contactFormSchema = z
   .object({
     name: z.string().min(1, { message: "Nome é obrigatório" }),
@@ -15,6 +18,7 @@ const contactFormSchema = z
       .string()
       .min(1, { message: "Mensagem é obrigatória" })
       .max(500, { message: "Sua mensagem deve conter até 500 caracteres" }),
+    access_key: z.string(),
   })
   .required();
 
@@ -22,11 +26,67 @@ export default function ContactForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    reset,
+    formState: { errors, isSubmitting },
   } = useForm({ resolver: zodResolver(contactFormSchema) });
 
+  const onSubmit = async (data, e) => {
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data, null, 2),
+      });
+
+      const responseBody = await response.json();
+
+      if (responseBody.success) {
+        toast("Mensagem enviada com sucesso", {
+          duration: 4000,
+          icon: "✅",
+          style: {
+            background: "rgb(134 239 172)",
+            color: "rgb(21 21 21)",
+            border: "1px solid rgb(21 21 21)",
+            padding: "8px",
+          },
+        });
+        e.target.reset();
+        reset();
+      } else {
+        console.log(responseBody.message);
+      }
+    } catch (error) {
+      toast("Erro ao enviar a mensagem", {
+        duration: 4000,
+        icon: "❌",
+        style: {
+          background: "rgb(255 111 91)",
+          color: "rgb(21 21 21)",
+          border: "1px solid rgb(21 21 21)",
+          padding: "8px",
+        },
+      });
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit} className="w-1/2 flex flex-col gap-8">
+    <form onSubmit={handleSubmit(onSubmit)} className="w-1/2 flex flex-col gap-8">
+      <Toaster position="top-right" />
+      <input
+        type="hidden"
+        value="a3c3048e-029a-49c3-805b-312915a41e36"
+        {...register("access_key")}
+      />
+      <input
+        type="checkbox"
+        id=""
+        className="hidden"
+        style={{ display: "none" }}
+        {...register("botcheck")}></input>
       <div className="bg-[#242424] flex flex-col">
         <label htmlFor="name" className="sr-only">
           Nome
@@ -37,10 +97,10 @@ export default function ContactForm() {
           id="name"
           placeholder="NOME"
           className={`bg-[#242424] w-full pl-6 pb-4 outline-none border-b border-gray text-input uppercase text-white placeholder:text-gray text-base -tracking-[0.22px] 
-            ${errors?.name ? "border-red" : "focus:border-green"}`}></input>
-        <p className="self-end p-1 text-xs -tracking=[0.17px] text-red">
-          {errors?.name?.message}
-        </p>
+            ${errors.name ? "border-red" : "focus:border-green"}`}></input>
+        {errors.name && (
+          <p className="self-end p-1 text-xs -tracking=[0.17px] text-red">{errors.name?.message}</p>
+        )}
       </div>
       <div className="bg-[#242424] flex flex-col">
         <label htmlFor="email" className="sr-only">
@@ -48,14 +108,16 @@ export default function ContactForm() {
         </label>
         <input
           {...register("email")}
-          type="email"
+          type="text"
           id="email"
           placeholder="EMAIL"
           className={`bg-[#242424] w-full pl-6 pb-4 outline-none border-b border-gray text-input uppercase text-white placeholder:text-gray text-base -tracking-[0.22px] 
-          ${errors?.email ? "border-red" : "focus:border-green"}`}></input>
-        <p className="self-end p-1 text-xs -tracking=[0.17px] text-red">
-          {errors?.email?.message}
-        </p>
+          ${errors.email ? "border-red" : "focus:border-green"}`}></input>
+        {errors.email && (
+          <p className="self-end p-1 text-xs -tracking=[0.17px] text-red">
+            {errors.email?.message}
+          </p>
+        )}
       </div>
       <div className="bg-[#242424] flex flex-col">
         <label htmlFor="message" className="sr-only">
@@ -67,15 +129,29 @@ export default function ContactForm() {
           id="message"
           placeholder="MENSAGEM"
           className={`bg-[#242424] w-full min-h-[110px] pl-6 pb-4 outline-none border-b border-gray text-input uppercase text-white placeholder:text-gray text-base -tracking-[0.22px] resize-none 
-          ${errors?.message ? "border-red" : "focus:border-green"}`}></textarea>
-        <p className="self-end p-1 text-xs -tracking=[0.17px] text-red">
-          {errors?.message?.message}
-        </p>
+          ${errors.message ? "border-red" : "focus:border-green"}`}></textarea>
+        {errors.message && (
+          <p className="self-end p-1 text-xs -tracking=[0.17px] text-red">
+            {errors.message?.message}
+          </p>
+        )}
       </div>
-      <input
+      <button
         type="submit"
-        value="ENVIAR MENSAGEM"
-        className="self-end cursor-pointer pb-3 w-fit text-white font-bold tracking-[2.3px] link link-underline text-base"></input>
+        disabled={isSubmitting}
+        className={`self-end uppercase cursor-pointer pb-3 w-fit text-white font-bold tracking-[2.3px] text-base
+        ${isSubmitting ? "pointer-events-none" : "link link-underline"}`}>
+        {isSubmitting ? (
+          <div className="flex justify-center items-center gap-2">
+            <span className="animate-spin">
+              <Spin />
+            </span>{" "}
+            <span>Enviando...</span>
+          </div>
+        ) : (
+          "Enviar mensagem"
+        )}
+      </button>
     </form>
   );
 }
