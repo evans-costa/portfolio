73748b40826b49cea900dfc8916367f0c1e5957f
Evandro Costa <evandro.souzac@gmail.com>
2024-03-25 13:51:49 -0300

    Fix header responsiveness

--- src/components/Sections/Header/index.js
+++ src/components/Sections/Header/index.js
@@ -2,7 +2,9 @@ import Social from "@/components/Social/";
 
 export default function Header() {
   return (
-    <header id="header" className="w-auto flex flex-col justify-between pt-4 md:pt-7 lg:pt-10">
+    <header
+      id="header"
+      className="w-auto max-w-screen-xl flex justify-between mx-auto px-4 md:px-7 xl:px-12 pt-4 md:pt-7 lg:pt-10">
       <Social />
     </header>
   );
