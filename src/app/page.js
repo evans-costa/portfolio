6e96eda467a2d6d4fb5eacf9217eb3828b2df270
Evandro Costa <evandro.souzac@gmail.com>
2024-03-25 13:41:57 -0300

    Put Header on layout page

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import Projects from "@/components/Sections/Projects";
 export default async function Home() {
   return (
     <main className="mx-auto overflow-x-hidden w-full max-w-screen-xl min-h-screen flex-col items-center justify-center md:justify-between px-4 md:px-7 xl:px-12 text-gray">
-      <Header />
       <Hero />
       <Experiences />
       <Projects />
