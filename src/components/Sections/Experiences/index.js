65066e8557079f51a61e80794c0c9d9509f81c7d
Evandro Costa <evandro.souzac@gmail.com>
2024-03-25 13:49:01 -0300

    Put a screen reader only heading

--- src/components/Sections/Experiences/index.js
+++ src/components/Sections/Experiences/index.js
@@ -6,8 +6,9 @@ export default function Experiences() {
     <section
       id="experiences"
       className="overflow-x-visible relative max-w-screen-xl mb-20 lg:mb-36 py-10 md:pt-14 xl:pt-16 border-t border-b md:border-b-0 border-white grid grid-cols-1 place-content-center justify-items-center gap-y-6 md:grid-cols-2 md:justify-items-start lg:grid-cols-3 md:gap-y-14">
+      <h1 className="sr-only">Experiências</h1>
       <ExperienceList />
-      <div className="absolute  md:top-96 lg:top-64 -right-[400px]">
+      <div className="absolute md:top-96 lg:top-64 -right-[400px]">
         <PatternRings />
       </div>
     </section>
