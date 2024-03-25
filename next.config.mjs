2009ae276bf84cf31b4604e0439aadef7a969d8b
Evandro Costa <evandro.souzac@gmail.com>
2024-03-25 13:37:22 -0300

    Set security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,50 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [{ source: "/(.*)", headers: securityHeaders }];
+  },
+};
+
+const ContentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    child-src 'self';
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'self';
+    connect-src *;
+    font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), autoplay=()",
+  },
+];
 
 export default nextConfig;
