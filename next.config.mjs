2228960eb457f0dcb8a0608b8e6ab478f8541f70
Evandro Costa <evandro.souzac@gmail.com>
2024-03-26 12:44:58 -0300

    Fix CSP headers for Google Analytics

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,16 @@ const nextConfig = {
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://*.googletagmanager.com;
     style-src 'self' 'unsafe-inline';
-    img-src www.googletagmanager.com 'self' blob: data:;
+    img-src 'self' 'unsafe-inline' www.googletagmanager.com https://*.google-analytics.com https://*.googletagmanager.com blob: data:;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
+    connect-src 'self' 'unsafe-inline' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com;
 `;
 
 const securityHeaders = [
