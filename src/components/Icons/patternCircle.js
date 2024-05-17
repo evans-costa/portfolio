fec947d9d476837c0266c50cfa26afb88dea6bb6
Evandro Costa <evandro.souzac@gmail.com>
2024-05-16 21:19:17 -0300

    Create profile picture and pattern Cirle image

---
+++ src/components/Icons/patternCircle.js
@@ -0,0 +1,14 @@
+export default function PatternCircle() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="129" height="129">
+      <circle
+        cx="830.5"
+        cy="585.5"
+        r="64"
+        fill="none"
+        stroke="#FFF"
+        transform="translate(-766 -521)"
+      />
+    </svg>
+  );
+}
